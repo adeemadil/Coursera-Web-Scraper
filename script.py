e214d98a45726617e94d8ca33888436e4c13b853
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2022-10-29 20:19:57 +0300

    edit: Changes made to scrape data for each job

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 # Importing Libraries
 # from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
 import requests
 
 # function to extract html document from given url
-
-
 def getHTMLdocument(url):
 
     # request for HTML document of given url
@@ -13,6 +12,16 @@ def getHTMLdocument(url):
 
     # response will be provided in JSON format
     return response.text
+
+#function to Remove non-ASCII characters
+def cleanText(temp_roles):
+    roles = []
+    for role in temp_roles:
+        # print(role)
+        text = role.get_text().strip()
+        res = re.sub(r'[^\x00-\x7F]+',' ', text)
+        roles.append( res)
+    return roles
 
 
 # CONSTANTS
@@ -32,20 +41,42 @@ print(urls)
 result = []
 
 for url in urls:
-    JOB_ID = url.split('gh_jid=')[1]
-    TOKEN = 'https://boards.greenhouse.io/embed/job_app?for=coursera&token={JOB_ID}'
-    RES_HTML = getHTMLdocument(url)
+
+    JOB_ID = urls[0].split('gh_jid=')[1]
+    TOKEN = 'https://boards.greenhouse.io/embed/job_app?for=coursera&token=' + str(JOB_ID)
+    RES_HTML = getHTMLdocument(TOKEN)
+
+
     B_SP = BeautifulSoup(RES_HTML, 'html.parser')
 
-    title = B_SP.find('h1', class_='app-title')
-    company = B_SP.find('span', class_='company-name')
-    location = B_SP.find('div', class_='location')
-    # job_overview = B_SP.find('')
-    # roles = B_SP.find('')
-    # basic_quali = B_SP.find('')
-    # pref_quali = B_SP.find('')
+    #Finding Title
+    title = B_SP.find('h1', class_='app-title').get_text().strip()
+
+    #Finding Company Name
+    company = B_SP.find('span', class_='company-name').get_text().strip().split(" ")[1]
+
+    #Finding Location
+    location = B_SP.find('div', class_='location').get_text().strip()
+
+    #Finding Overview of Job
+    job_overview = B_SP.select('#content p:nth-of-type(2)')[1].get_text().strip()
+
+    #Finding Responsibilities
+    temp_roles = B_SP.select('#content ul:first-of-type li')
+    roles = cleanText(temp_roles)
+
+    #Finding Basic Qualifications
+    temp_basic_quali = B_SP.select('#content ul:nth-of-type(2) li')
+    basic_quali = cleanText(temp_basic_quali)
+
+    #Finding Preffered Qualifications
+    temp_pref_quali = B_SP.select('#content ul:nth-of-type(3) li')
+    pref_quali = cleanText(temp_pref_quali)
 
     temp = {'Title': title, 'Company' : company, 'Location': location, 'Job Overview': job_overview,
         'Roles': roles, 'Basic Qualifications': basic_quali, 'Preffered Qualifications': pref_quali}
-    
+
     result.append(temp)
+
+print(result)
+    
